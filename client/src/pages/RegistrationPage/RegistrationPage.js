de298245b26839bb52b3ea26fa2a5e29edc50ee8
Vlad Svistielnik <svistv96@gmail.com>
2020-04-02 20:46:04 +0300

    some changes in json for register page

--- client/src/pages/RegistrationPage/RegistrationPage.js
+++ client/src/pages/RegistrationPage/RegistrationPage.js
@@ -43,15 +43,6 @@ const RegistrationPage = (props) => {
                 <div className={styles.articlesMainContainer}>
                     <div className={styles.ColumnContainer}>
                         {renderArticles}
-                        <div className={styles.headerArticle}>I have other questions! How can I get in touch with
-                            Squadhelp?
-                        </div>
-                        <div className={styles.article}>
-                            Check out our <span className={styles.orangeSpan}>FAQs</span> or send us a <span
-                            className={styles.orangeSpan}>message</span>. For assistance with launching a contest,
-                            you can also call us at (877) 355-3585 or schedule a <span
-                            className={styles.orangeSpan}>Branding Consultation</span>
-                        </div>
                     </div>
                 </div>
             </div>
