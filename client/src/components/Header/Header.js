606576b889c0de2682a0d24af83cf0e4ded14856
Vlad Svistielnik <svistv96@gmail.com>
2020-04-02 11:33:42 +0300

    Change: remove changeRoute prop && add articles info in json

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -70,7 +70,7 @@ class Header extends React.Component {
                 <div className={styles.loginSignnUpHeaders}>
                     <div className={styles.numberContainer}>
                         <img src={`${CONSTANTS.STATIC_IMAGES_PATH}phone.png`} alt='phone'/>
-                        <span>(877)&nbsp;355-3585</span>
+                        <a href={'tel: (877)&nbsp;355-3585'}>(877)&nbsp;355-3585</a>
                     </div>
                     <div className={styles.userButtonsContainer}>
                         {this.renderLoginButtons()}
